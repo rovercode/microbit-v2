396a02dea87757d5f9190c3fe6a1821d04b816f2
Clifton Barnes <clifton.barnes@rovercode.com>
2021-07-19 10:59:57 -0400

    Add main program

--- source/main.cpp
+++ source/main.cpp
@@ -1,12 +1,246 @@
+#include <stdio.h>
+
 #include "MicroBit.h"
+#include "MicroBitUARTService.h"
+
+#include "inc/drivers/Bot.h"
+#include "inc/drivers/gigglebot/Gigglebot.h"
+#include "inc/drivers/cutebot/Cutebot.h"
 
 MicroBit uBit;
+MicroBitUARTService *uart;
 
-int 
-main()
+Bot* bot;
+
+const MicroBitImage IMAGE_HAPPY = MicroBitImage("0,0,0,0,0\n0,1,0,1,0\n0,0,0,0,0\n1,0,0,0,1\n0,1,1,1,0\n");
+
+int connected = 0;
+
+void onConnected(MicroBitEvent)
+{
+    uBit.display.print(IMAGE_HAPPY);
+    connected = 1;
+    uart->eventOn(ManagedString("\n"), ASYNC);
+}
+
+void handleDisplay()
+{
+    ManagedString msg = uart->readUntil(ManagedString("\n"));
+    uBit.display.printAsync(msg, 500);
+    uBit.display.print(IMAGE_HAPPY);
+}
+
+void handleRightMotor()
+{
+    ManagedString msg = uart->readUntil(ManagedString("\n"));
+    bot->setRightMotorPower(atoi(msg.toCharArray()));
+}
+
+void handleLeftMotor()
+{
+    ManagedString msg = uart->readUntil(ManagedString("\n"));
+    bot->setLeftMotorPower(atoi(msg.toCharArray()));
+}
+
+void handleBothMotors()
+{
+    ManagedString msg = uart->readUntil(ManagedString("\n"));
+    bot->setBothMotorsPower(atoi(msg.toCharArray()));
+}
+
+void parseRgb(uint8_t *red, uint8_t *green, uint8_t *blue)
+{
+    ManagedString redString = uart->readUntil(ManagedString(":"));
+    *red = atoi(redString.toCharArray());
+    ManagedString greenString = uart->readUntil(ManagedString(":"));
+    *green = atoi(greenString.toCharArray());
+    ManagedString blueString = uart->readUntil(ManagedString("\n"));
+    *blue = atoi(blueString.toCharArray());
+}
+
+void handleLeftHeadlight()
+{
+    uint8_t red, green, blue;
+    parseRgb(&red, &green, &blue);
+    bot->setLeftHeadlightColor(red, green, blue);
+}
+
+void handleRightHeadlight()
+{
+    uint8_t red, green, blue;
+    parseRgb(&red, &green, &blue);
+    bot->setRightHeadlightColor(red, green, blue);
+}
+
+void handleBothHeadlights()
+{
+    uint8_t red, green, blue;
+    parseRgb(&red, &green, &blue);
+    bot->setBothHeadlightColor(red, green, blue);
+}
+
+void onUartEvent(MicroBitEvent)
+{
+    ManagedString msg = uart->readUntil(ManagedString(":"), ASYNC);
+    if (msg.length() == 0)
+    {
+        // Invalid command. Throw out rx buffer.
+        uart->readUntil(ManagedString("\n"));
+        return;
+    }
+
+    if (msg == "disp")
+    {
+        create_fiber(handleDisplay);
+    }
+    else if (msg == "right-motor") {
+        create_fiber(handleRightMotor);
+    }
+    else if (msg == "left-motor") {
+        create_fiber(handleLeftMotor);
+    }
+    else if (msg == "both-motors") {
+        create_fiber(handleBothMotors);
+    }
+    else if (msg == "left-hl") {
+        create_fiber(handleLeftHeadlight);
+    }
+    else if (msg == "right-hl") {
+        create_fiber(handleRightHeadlight);
+    }
+    else if (msg == "both-hl") {
+        create_fiber(handleBothHeadlights);
+    }
+    else
+    {
+        // Unknown command. Throw out rx buffer.
+        uart->readUntil(ManagedString("\n"));
+    }
+}
+
+void onDisconnected(MicroBitEvent)
+{
+    uBit.display.print("R");
+    connected = 0;
+}
+
+void onButtonA(MicroBitEvent)
+{
+    if (connected == 0)
+    {
+        return;
+    }
+    uart->send(ManagedString("button:a"), ASYNC);
+}
+
+void onButtonB(MicroBitEvent)
+{
+    if (connected == 0)
+    {
+        return;
+    }
+    uart->send(ManagedString("button:b"), ASYNC);
+}
+
+void onNewAccelData(MicroBitEvent)
+{
+    if (connected == 0)
+    {
+        return;
+    }
+    uBit.sleep(1);  // Prevents an 020 error. 🤷
+    Sample3D sample = uBit.accelerometer.getSample();
+    char buffer[20];
+    snprintf(buffer, sizeof(buffer), "accel:%d,%d,%d", sample.x, sample.y, sample.z);
+    uart->send(buffer, ASYNC);
+}
+
+void onNewBatteryData(MicroBitEvent)
+{
+    if (connected == 0)
+    {
+        return;
+    }
+    uBit.sleep(1);  // Prevents an 020 error. 🤷
+    char buffer[20];
+    snprintf(buffer, sizeof(buffer), "battery-sens:%d", bot->getBatteryVoltage());
+    uart->send(buffer, ASYNC);
+}
+
+void onNewLightSensorsData(MicroBitEvent)
+{
+    if (connected == 0)
+    {
+        return;
+    }
+    uBit.sleep(1);  // Prevents an 020 error. 🤷
+    char buffer[23];
+    snprintf(buffer, sizeof(buffer), "light-sens:%d,%d", bot->getLeftLightSensorReading(), bot->getRightLightSensorReading());
+    uart->send(buffer, ASYNC);
+}
+
+void onNewLineSensorsData(MicroBitEvent)
+{
+    if (connected == 0)
+    {
+        return;
+    }
+    uBit.sleep(1);  // Prevents an 020 error. 🤷
+    char buffer[23];
+    snprintf(buffer, sizeof(buffer), "line-sens:%d,%d", bot->getLeftLineSensorReading(), bot->getRightLineSensorReading());
+    uart->send(buffer, ASYNC);
+}
+
+void onNewDistanceSensorData(MicroBitEvent)
+{
+    if (connected == 0)
+    {
+        return;
+    }
+    uBit.sleep(1);  // Prevents an 020 error. 🤷
+    char buffer[23];
+    snprintf(buffer, sizeof(buffer), "dist-sens:%d", bot->getDistanceSensorReading());
+    uart->send(buffer, ASYNC);
+}
+
+int main()
 {
     uBit.init();
 
-    while(1)
-        uBit.display.scroll("HELLO WORLD!");
+    bot = new Gigglebot(uBit.i2c);
+    if (!bot->isDetected()) {
+        delete bot;
+        bot = new Cutebot(uBit.i2c, uBit.io);
+        if (!bot->isDetected()) {
+            delete bot;
+            uBit.display.scrollAsync("NO BOT");
+        }
+    }
+
+    uBit.accelerometer.setPeriod(1000);  // milliseconds
+
+    uBit.messageBus.listen(MICROBIT_ID_BLE, MICROBIT_BLE_EVT_CONNECTED, onConnected);
+    uBit.messageBus.listen(MICROBIT_ID_BLE, MICROBIT_BLE_EVT_DISCONNECTED, onDisconnected);
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
+    uBit.messageBus.listen(MICROBIT_ID_BLE_UART, MICROBIT_BLE_EVENT_SERVICE, onUartEvent);
+    // uBit.messageBus.listen(MICROBIT_ID_ACCELEROMETER, MICROBIT_ACCELEROMETER_EVT_DATA_UPDATE, onNewAccelData);
+    uBit.messageBus.listen(BOT_ID_BATTERY, BOT_BATTERY_EVT_UPDATE, onNewBatteryData);
+    uBit.messageBus.listen(BOT_ID_LIGHT_SENSORS, BOT_LIGHT_SENSORS_EVT_UPDATE, onNewLightSensorsData);
+    uBit.messageBus.listen(BOT_ID_LINE_SENSORS, BOT_LINE_SENSORS_EVT_UPDATE, onNewLineSensorsData);
+    uBit.messageBus.listen(BOT_ID_DISTANCE_SENSOR, BOT_DISTANCE_SENSOR_EVT_UPDATE, onNewDistanceSensorData);
+
+    // Note GATT table size increased from default in MicroBitConfig.h
+    // #define MICROBIT_SD_GATT_TABLE_SIZE             0x500
+    uart = new MicroBitUARTService(*uBit.ble, 32, 128);
+
+    uBit.display.print("R");
+    uBit.sleep(2000);
+    uBit.display.scrollAsync(microbit_friendly_name());
+    uBit.display.print("R");
+
+    // If main exits, there may still be other fibers running or registered event handlers etc.
+    // Simply release this fiber, which will mean we enter the scheduler. Worse case, we then
+    // sit in the idle task forever, in a power efficient sleep.
+    release_fiber();
 }
